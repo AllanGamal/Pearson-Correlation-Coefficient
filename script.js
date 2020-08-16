ee3518e6ad9138babd8cefd9c31f26ea2521f510
AllanGamal <63316423+AllanGamal@users.noreply.github.com>
2020-08-16 21:48:26 +0200

    table added with add button

---
+++ script.js
@@ -0,0 +1,26 @@
+let rows = [];
+let colums = [0, 1];
+console.log(rows.length);
+
+// Add row and id datapoints
+function addRow() {
+  let count;
+  let table = document.querySelector("table");
+  let row = table.insertRow(1);
+
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+
+  for (let i = 0; i <= rows.length; i++) {
+    if (!rows.includes(i)) {
+      rows.push(i);
+      cell1.innerHTML = '<input type="text" class="c1">';
+      cell2.innerHTML = "<input type='text' class='c2'>";
+
+      row.id = i;
+      break;
+    }
+  }
+}
+
+document.getElementById("add-row").addEventListener("click", addRow);
